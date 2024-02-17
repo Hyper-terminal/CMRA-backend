b20fcd345596746bccd410abc54dd39a36bfb7b6
abhi <abhisharma001001001@gmail.com>
2024-02-17 22:52:42 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import { userRoutes } from "./routes";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use('/users', userRoutes);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
