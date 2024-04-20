c8ee148912a2aea1c3f6427cc711cbfe95340026
Abhishek <a.kalonia@mrikal.com>
2024-04-20 13:09:42 +0530

    Fix: Worker types in controllers

--- src/controllers/worker.controller.ts
+++ src/controllers/worker.controller.ts
@@ -1,8 +1,9 @@
 import { Response } from "express";
-import { IRequest } from "../middlewares";
-import Worker from "../models/worker.model";
-import Task from "../models/task.model";
 import ResponseHandler from "../libs";
+import { IRequest } from "../middlewares";
+import Task from "../models/task.model";
+import Worker from "../models/worker.model";
+import { IWorker } from "../types/worker.type";
 
 export const createWorker = async (req: IRequest, res: Response) => {
   try {
@@ -46,7 +47,7 @@ export const createWorker = async (req: IRequest, res: Response) => {
 export const getAllWorkers = async (req: IRequest, res: Response) => {
   try {
     // extract query params
-    const { search } = req.query;
+    const { search, page = 1, pageSize = 20 } = req.query;
     let query = {};
 
     if (search) {
@@ -54,12 +55,16 @@ export const getAllWorkers = async (req: IRequest, res: Response) => {
         $or: [{ name: { $regex: new RegExp(search as string, "i") } }],
       };
     }
-    // const skip: number = ((page as number) - 1) * (pageSize as number);
 
-    const results = Worker.find(query).populate({
-      path: "tasks",
-      model: Task,
-    });
+    const skip = ((page as number) - 1) * parseInt(pageSize as any);
+
+    const results: IWorker[] = await Worker.find(query)
+      .populate({
+        path: "tasks",
+        model: Task,
+      })
+      .skip(skip)
+      .limit(parseInt(pageSize as string));
 
     return ResponseHandler.success(res, "", results);
   } catch (err) {
--- src/routes/worker.route.ts
+++ src/routes/worker.route.ts
@@ -5,6 +5,6 @@ import { requireSignin } from "../middlewares";
 const router = express.Router();
 
 router.post("/create", requireSignin as any, createWorker as any);
-router.get("/", requireSignin , getAllWorkers)
+router.get("/", requireSignin as any, getAllWorkers as any);
 
 export default router;
