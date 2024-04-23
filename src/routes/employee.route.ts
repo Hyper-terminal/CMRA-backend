91bf70e1b83b10d0653e76e57ded5b36ca871de1
Abhishek <a.kalonia@mrikal.com>
2024-04-23 13:04:20 +0530

    Fix: Correct employee details in api

--- src/routes/employee.route.ts
+++ src/routes/employee.route.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { createWorker, getAllWorkers } from "../controllers/worker.controller";
+import {
+  createEmployee,
+  getAllEmployees,
+  getTotalSalary,
+} from "../controllers/employee.controller";
 import { requireSignin } from "../middlewares";
-import { getTotalSalary } from "../controllers/employee.controller";
 
 const router = express.Router();
 
-router.post("/create", requireSignin as any, createWorker as any);
-router.get("/", requireSignin as any, getAllWorkers as any);
+router.post("/create", requireSignin as any, createEmployee as any);
+router.get("/", requireSignin as any, getAllEmployees as any);
 router.get("/totalSalary", requireSignin as any, getTotalSalary as any);
 
 export default router;
