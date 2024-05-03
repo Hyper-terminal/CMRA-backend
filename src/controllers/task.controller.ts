0e245ae19a3efe04b2653bcb695db6c00c473f32
Abhishek <a.kalonia@mrikal.com>
2024-05-03 11:41:25 +0530

    Removed the otp logic from startTask controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,5 +1,3 @@
-import bcrypt from "bcrypt";
-import { randomBytes } from "crypto";
 import { Request, Response } from "express";
 import ResponseHandler from "../libs";
 import Task from "../models/task.model";
@@ -60,20 +58,8 @@ export const startTask = async (req: Request, res: Response) => {
       return ResponseHandler.error(res, 404, "Task not found");
     }
 
-    // Generate a secure random OTP using crypto
-    // const otp = randomBytes(3).toString("hex").slice(0, 4);
-    const otp = randomBytes(2)
-      .readUInt16BE()
-      .toString()
-      .padStart(4, "0")
-      .slice(0, 4);
-
-    // Hash the OTP
-    const hashedOtp = await bcrypt.hash(otp, 10);
-
     // Update the task with the hashed OTP
     task.status = "Started";
-    task.otp = hashedOtp;
     await task.save();
 
     // Send the OTP to the client (implementation depends on your setup)
