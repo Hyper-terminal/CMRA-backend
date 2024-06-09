5fcb58c32056eaa85022eda9208ec592a9505aa2
Abhishek <a.kalonia@mrikal.com>
2024-06-09 16:44:00 +0530

    removed twilio

--- readme.md
+++ readme.md
@@ -1,14 +1,21 @@
 # Project Name: CMRA
 
 ## Description
+
 This project is developed to assist companies in management and handling tasks.
 
 ## Environment Variables (.env)
+
 - MONGO_URI=
 - PORT=
 - SECRET=
+- TWILIO_ACCOUNT_SID=
+- TWILIO_AUTH_TOKEN=
+- TWILIO_PHONE=
+- REFRESH_TOKEN_SECRET=
 
 ## Technologies Used
+
 - Node.js
 - Express
 - TypeScript
@@ -18,16 +25,16 @@ This project is developed to assist companies in management and handling tasks.
 
 - **CMRA/**
   - **routes/**: Contains route definitions.
-    - *...*
+    - _..._
   - **middlewares/**: Middleware functions.
-    - *...*
+    - _..._
   - **controllers/**: Controller functions.
-    - *...*
+    - _..._
   - **libs/**: External libraries.
-    - *...*
+    - _..._
   - **models/**: Database models.
-    - *...*
-
+    - _..._
 
 ## Package Manager
+
 - Yarn
--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,7 +1,6 @@
 import bcrypt from "bcrypt";
 import { randomBytes } from "crypto";
 import { Request, Response } from "express";
-import { Worker } from "worker_threads";
 import ResponseHandler from "../libs";
 import Task from "../models/task.model";
 import { ITask } from "../types/task.type";
@@ -79,18 +78,7 @@ export const startTask = async (req: Request, res: Response) => {
   }
 };
 
-const sendOtpToClient = async (phone: number, otp: string) => {
-  const worker = new Worker("../libs/twilio.ts", {
-    workerData: { phone, otp },
-  });
-  worker.on("message", (message) =>
-    console.log("Message from worker:", message)
-  );
-  worker.on("error", (error) => console.error("Error from worker:", error));
-  worker.on("exit", (code) => {
-    if (code !== 0) console.error(`Worker stopped with exit code ${code}`);
-  });
-};
+const sendOtpToClient = async (phone: number, otp: string) => {};
 
 export const generateOtp = async (req: Request, res: Response) => {
   try {
@@ -112,6 +100,7 @@ export const generateOtp = async (req: Request, res: Response) => {
     task.otp = hashedOtp;
     task.otpExpires = otpExpires;
     await task.save();
+    console.log("Saving OTP to task");
     sendOtpToClient(task.clientPhone, otp);
 
     return ResponseHandler.success(res, "OTP generated successfully", { otp });
